61d949c2beee8629fc9d35288f1ca10d5331cbea
Egor <43146047+cheerfulperson@users.noreply.github.com>
2023-02-26 23:02:08 +0300

    refactor: update spacing

--- firstCode.asm
+++ firstCode.asm
@@ -1,7 +1,7 @@
 	.include "m328pdef.inc"
 	.dseg
 
-	.def quotient = r16      // частное
+	.def quotient = r16 // quotient
 	.def xL = r26
 	.def xH = r27
 	.def yL = r28
@@ -9,15 +9,15 @@
 	.def zL = r30
 	.def zH = r31
 
-	.equ divider = 0x02      // делитель
+	.equ divider = 0x02 // divisor
 	.equ iNumber = 0x12E4
 	.equ sNumber = 0x00E8
 
 	.cseg
-	.org	0x00
+	.org 0x00
 
-	ldi xL, low(iNumber)    //младший байт
-	ldi xH, high(iNumber)   //старший байт
+	ldi xL, low(iNumber) //low byte
+	ldi xH, high(iNumber) // high byte
 
 	ldi yL, low(sNumber)
 	ldi yH, high(sNumber)
@@ -27,18 +27,18 @@ sub_numbers:
 	sbc xH, yH
 	mov zL, xL
 
-div:   
-	clc                     // очищаем carry-bit
-    clr   quotient
-	subi  zL, divider
-    inc   quotient         
-    brcc  PC-2              // до тех пор пока разность R17-R16 > 0 (переход, если перенос сброшен)
-    dec   quotient          // когда разность zL, divider < 0
-	clr   zl
-    mov   zL, quotient      // восстанавливаем quotient и корректируем zL 
+div:
+	clc // clear carry-bit
+	clr quotient
+	subi zL, divider
+	inc quotient
+	brcc PC-2 // as long as R17-R16 difference > 0 (jump if carry is reset)
+	dec quotient // when difference zL, divider < 0
+	clr zl
+	mov zL, quotient // restore quotient and adjust zL
 
 end:
-    sts 0x25, xL
+	sts 0x25, xL
 	sts 0x30, xH
     
 loop: rjmp loop
